69731608b9b56c398115fb96dd7d88c5a9bdf593
Anas <anasmalik19997886@gmail.com>
2023-12-21 18:01:49 +0530

    Add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,19 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store.js";
+import  { persistor,store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+
+
+
+// persist is using for storing data, if we are refreshing the page then the data gone but after using persist then the data is storing in "Local Storage"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <Provider store={store}>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
+    </PersistGate>
+  </Provider>
   // {/* </React.StrictMode> */}
 );
