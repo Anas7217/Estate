95a35bb7fc06215891815876963d6b25b0e00198
Anas <anasmalik19997886@gmail.com>
2023-12-21 15:08:59 +0530

    Add redux toolkit

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,58 +1,55 @@
-import React, { useState } from 'react'
-import { useNavigate,Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+} from "../redux/user/userSlice";
 
 const Signin = () => {
   const [formData, setFormData] = useState({});
-  const [error,setError]=useState(null)
-  const [loading,setLoading]=useState(false)
-  // const [popup,setPopup]=useState(false)
-  const navigate=useNavigate()
 
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector((state) => state.user);
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
   const handleSubmit = async (e) => {
     e.preventDefault(); // As we click submit button then the page is not refreshing
-    console.log('Request data:', JSON.stringify(formData));
-   
-   try {
-     setLoading(true);
-     const res = await fetch('/api/auth/signin', {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify(formData),
-     });
-     const data = await res.json();
-     console.log(data);
-     if(data.success===false){
-         setLoading(false)
-         setError(data.message)
-         return;
-     }
-     setLoading(false)
-     setError(null)
-     navigate('/')
-    //  setPopup(true)
-   } catch (error) {
+    console.log("Request data:", JSON.stringify(formData));
 
-    setLoading(false)
+    try {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        return;
+      }
 
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error.message));
 
-      console.log(error)
-    
-   }
- 
-  
+      console.log(error);
+    }
   };
-  // console.log(formData)
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-       
         <input
           type="email"
           placeholder="email"
@@ -66,10 +63,12 @@ const Signin = () => {
           className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
-
         />
-        <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-          {loading?'Loading...':'Signin'}
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Signin"}
         </button>
         {/* <button  disabled className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Signup</button> */}
       </form>
@@ -84,9 +83,8 @@ const Signin = () => {
       {/* {
         popup && <p className="text-red-500 mt-5">user created successfully</p>
       } */}
-     
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
