0672ef811fc7a02d60bcadf3cc49fe2838f529ca
Anas <anasmalik19997886@gmail.com>
2023-12-28 11:59:55 +0530

    Add signout unctionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -86,3 +86,14 @@ export const google = async (req, res, next) => {
     next(error);
   }
 };
+
+
+export const signOut=async(req,res)=>{
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json("log out successfully!")
+    
+  } catch (error) {
+      next(error)
+  }
+}
