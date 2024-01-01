69e94cd31fcf839e4ae6dabd66b23dadaa208729
Anas <anasmalik19997886@gmail.com>
2024-01-01 13:04:30 +0530

    Add upload images functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,4 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import app from "../firebase.js";
+const [files, setFiles] = useState([]);
+const [formData, setFormData] = useState({
+  imageUrls: [],
+});
+const [imageUploadError, setImageUploadError] = useState(false);
+
+const handleImageSubmit = (e) => {
+  // e.preventDefault()
+  if (files.length > 0 && files.length + formData.imageUrls.length < 7) {
+    const promises = [];
+    for (let i = 0; i < files.length; i++) {
+      promises.push(storeImage(files[i]));
+    }
+    Promise.all(promises)
+      .then((urls) => {
+        setFormData({
+          ...formData,
+          imageUrls: formData.imageUrls.concat(urls),
+        });
+        setImageUploadError(false);
+      })
+      .catch((error) => {
+        setImageUploadError("Image upload failed (2 mb max per image)");
+        setUploading(false);
+      });
+  } else {
+    setImageUploadError("You can only upload 6 images per listing");
+    setUploading(false);
+  }
+};
+
+const handleRemoveImage=(index)=>{
+  setFormData({
+    ...formData,
+    imageUrls: formData.imageUrls.filter((_,i)=>i!==index),
+
+  })
+
+
+}
+
+const storeImage = async (file) => {
+  return new Promise((resolve, reject) => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log(`Upload is ${progress}% done`);
+      },
+      (error) => {
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          resolve(downloadURL);
+        });
+      }
+    );
+  });
+};
 
 const CreateListing = () => {
   return (
@@ -89,7 +161,9 @@ const CreateListing = () => {
               <span>Offer</span>
             </div>
           </div>
-           <div className="flex flex-wrap gap-6">   {/* flex wrap is used for wrapping the items (if they are going outside the container) */}
+          <div className="flex flex-wrap gap-6">
+            {" "}
+            {/* flex wrap is used for wrapping the items (if they are going outside the container) */}
             <div className="flex items-center gap-2">
               <input
                 type="number"
@@ -143,7 +217,7 @@ const CreateListing = () => {
                   max="10000000"
                   required
                   className="p-3 border border-gray-300 rounded-lg"
-                //   onChange={handleChange}
+                  //   onChange={handleChange}
                   value={formData.discountPrice}
                 />
                 <div className="flex flex-col items-center">
@@ -166,7 +240,7 @@ const CreateListing = () => {
           </p>
           <div className="flex gap-4">
             <input
-            //   onChange={(e) => setFiles(e.target.files)}
+              onChange={(e) => setFiles(e.target.files)}
               className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
