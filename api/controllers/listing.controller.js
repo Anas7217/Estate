e1faf035355207dbe62a7742226f386ff4af307f
Anas <anasmalik19997886@gmail.com>
2024-01-13 14:09:55 +0530

    Add search Url functionality

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import { query } from "express";
 import Listing from "../models/listing.model.js"
 import { errorHandler } from "../utils/error.js";
 
@@ -73,4 +74,55 @@ export const getListing=async(req,res,next)=>{
     
    }
 
+}
+
+
+//search
+export const getListings=async(req,res,next)=>{
+    try {
+        const limit=parseInt(req.query.limit) || 9;
+        const startIndex=parseInt(req.query.startIndex) || 0;
+        let offer=req.query.offer;
+
+        if(offer===undefined || offer==='false'){
+            offer={$in:[false,undefined]}   // in is used for value searching in dataase
+        }
+
+        let furnished=req.query.furnished;
+        if(furnished===undefined || furnished==='false'){
+            furnished={$in:[false,undefined]}   
+
+        }
+        let parking=req.query.parking;
+        if(parking===undefined || parking==='false'){
+            parking={$in:[false,undefined]}   
+
+        }
+        let type=req.query.type;
+        if(type===undefined || type==='all'){
+            parking={$in:['sale','rent']}   
+
+        }
+
+        const searchTerm=req.query.searchTerm || '';
+        const sort=req.query.sort || 'createdAt';
+        const order=req.query.order || 'desc';
+
+        const listings=await Listing.find({
+            name:{$regex:searchTerm,$options:'i'},  // 'i' dont care about lowercase or upppercase
+            offer,
+            furnished,
+            parking,
+            type
+        }).sort({
+            [sort]:order
+        }).limit(limit).skip(startIndex)
+
+        return res.status(200).json(listings)
+
+
+        
+    } catch (error) {
+        next(error)
+    }
 }
