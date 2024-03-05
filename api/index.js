54c0f42915731bb0283cea9c56ff958b11b43720
Anas <anasmalik19997886@gmail.com>
2024-03-05 22:10:32 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
+import path from "path";
 dotenv.config()
 
 // MONGO_URL=mongodb+srv://anasmalik1999786:ysxtN2D1IENEPkMm@cluster0.ksqqdp3.mongodb.net/stock
@@ -18,6 +19,8 @@ mongoose.connect(process.env.MONGO_URL)
     console.log(error,"database Error")
 })
 
+const __dirname=path.resolve();
+
 
 const app=express()
 
@@ -29,6 +32,12 @@ app.use(cookieParser())
 app.use("/api/user",userRouter)
 app.use("/api/auth",authRouter)
 app.use("/api/listing",listingRouter)
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))   // /client/dist/index.html
+})
 
 
 app.use((err,req,res,next)=>{
